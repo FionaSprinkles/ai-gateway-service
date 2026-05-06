94a2c399e7b7cab89a20121286adb08d81e9aea7
Fiona Friberg <fionafriberg@outlook.com>
2026-05-06 16:34:21 +0200

    Initial setup

---
+++ src/main/java/com/github/fionasprinkles/aigatewayservice/AiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.fionasprinkles.aigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGatewayServiceApplication.class, args);
+    }
+
+}
