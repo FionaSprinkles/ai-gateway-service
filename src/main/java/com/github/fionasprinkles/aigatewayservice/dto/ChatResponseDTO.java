15d102ad1b1f0fc5fd38de8b183e73706955a8f3
Fiona Friberg <fionafriberg@outlook.com>
2026-05-10 10:24:07 +0200

    add: Constructor and lombok data

--- src/main/java/com/github/fionasprinkles/aigatewayservice/dto/ChatResponseDTO.java
+++ src/main/java/com/github/fionasprinkles/aigatewayservice/dto/ChatResponseDTO.java
@@ -1,8 +1,13 @@
 package com.github.fionasprinkles.aigatewayservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  *Represents my API response
  */
+@AllArgsConstructor
+@Data
 public class ChatResponseDTO {
 
     private String response;
