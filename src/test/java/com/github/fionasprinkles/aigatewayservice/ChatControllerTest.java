42eca559d7f03e2447b607f6fec981be9bd2cf61
Fiona Friberg <fionafriberg@outlook.com>
2026-05-11 21:30:54 +0200

    create ChatControllerTest

---
+++ src/test/java/com/github/fionasprinkles/aigatewayservice/ChatControllerTest.java
@@ -0,0 +1,10 @@
+package com.github.fionasprinkles.aigatewayservice;
+
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(ChatController.class)
+class ChatControllerTest {
+
+}
