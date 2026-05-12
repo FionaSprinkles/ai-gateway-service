6125d0c7300f0b952784f60d19b9168829eed4f7
Fiona Friberg <fionafriberg@outlook.com>
2026-05-12 08:44:08 +0200

    refactor: add no args constructor to DTOs

--- src/main/java/com/github/fionasprinkles/aigatewayservice/dto/ChatRequestDTO.java
+++ src/main/java/com/github/fionasprinkles/aigatewayservice/dto/ChatRequestDTO.java
@@ -1,7 +1,11 @@
 package com.github.fionasprinkles.aigatewayservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ChatRequestDTO {
 
--- src/main/java/com/github/fionasprinkles/aigatewayservice/dto/ChatResponseDTO.java
+++ src/main/java/com/github/fionasprinkles/aigatewayservice/dto/ChatResponseDTO.java
@@ -2,9 +2,11 @@ package com.github.fionasprinkles.aigatewayservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /** Represents my API response */
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ChatResponseDTO {
 
