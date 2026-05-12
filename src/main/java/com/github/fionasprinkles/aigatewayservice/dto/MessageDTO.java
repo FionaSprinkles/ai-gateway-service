8a6a843bf771229b20aba0fa982cbf31fb701962
Fiona Friberg <fionafriberg@outlook.com>
2026-05-12 15:50:48 +0200

    Add Swagger documentation to DTOs

--- src/main/java/com/github/fionasprinkles/aigatewayservice/dto/MessageDTO.java
+++ src/main/java/com/github/fionasprinkles/aigatewayservice/dto/MessageDTO.java
@@ -1,12 +1,19 @@
 package com.github.fionasprinkles.aigatewayservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Single chat message")
 public class MessageDTO {
 
+  @Schema(description = "Role of the message sender")
   private String role;
+
+  @Schema(description = "Message content")
   private String content;
 }
