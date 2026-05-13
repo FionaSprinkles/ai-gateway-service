639f0ae403d6593ed807aaa854d7547b61dfe41d
Fiona Friberg <fionafriberg@outlook.com>
2026-05-13 11:06:55 +0200

    test: add message to session test

--- src/test/java/com/github/fionasprinkles/aigatewayservice/SessionMemoryServiceTest.java
+++ src/test/java/com/github/fionasprinkles/aigatewayservice/SessionMemoryServiceTest.java
@@ -30,4 +30,22 @@ class SessionMemoryServiceTest {
 
         assertTrue(messages.isEmpty());
     }
+
+    /**
+     * Verifies that a message can be added to a session.
+     */
+    @Test
+    void shouldAddMessageToSession() {
+
+        MessageDTO message =
+                new MessageDTO("user", "Hello");
+
+        sessionMemoryService.addMessage("session-123", message);
+
+        List<MessageDTO> messages =
+                sessionMemoryService.getMessages("session-123");
+
+        assertEquals(1, messages.size());
+        assertEquals("Hello", messages.getFirst().getContent());
+    }
 }
